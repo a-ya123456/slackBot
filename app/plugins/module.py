6149858fcd4087284fc221700f4ed87a63023ac8
a-ya123456 <a_ya_123456@yahoo.co.jp>
2020-12-23 15:55:02 +0900

    一部ファイルの削除

--- app/plugins/module.py
+++ app/plugins/module.py
@@ -6,21 +6,29 @@ from plugins import filewriter
 target_users = ['c0118010b9']
 target_channels = ['bot作成']
 
-@listen_to('^(.*)$')
-def comment_log(message,text):
+#@listen_to('^[\s\S]*.$')
+@default_reply()
+def comment_log(message):
+    #messageから抜き出して変数へ格納
     real_name = message.user['real_name']
     display_name = message.user['profile']['display_name']
     channel = message.channel._body['name']
+    text = message.body['text']
+    #target以外は記録しない
     if real_name not in target_users:
         return
     if channel not in target_channels:
         return
+
     f = filewriter.FileReadWrite()
     action = f.file_write("./logs/test.txt", text)
     message.react(action)
     message.send(text)
 
 
-@respond_to('^(.*)$')
-def comment_log2(message,text,):
+@respond_to('^[\s\S]*.$')
+def comment_log2(message):
+    text = message.body['text']
+    message.react('ok')
+    print(text)
     message.send(text)
